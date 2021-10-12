a49d7b6a56ab78e9e83f9fe4753d4c0ace04aad8
Tim Bijnen <bezigebijnen@gmail.com>
2021-10-12 22:52:16 +0200

    Fix

--- src/Socket/Socket.tsx
+++ src/Socket/Socket.tsx
@@ -3,14 +3,13 @@ import socketIOClient from "socket.io-client";
 import { useToasts } from "react-toast-notifications"
 
 
-const ENDPOINT = "http://127.0.0.1:5000";
 
 function App() {
   // const [response, setResponse] = useState("");
   const { addToast } = useToasts()
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
+    const socket = socketIOClient();
     socket.on("log", data => {
       const appearance = ['error', 'info', 'success', 'warning'].includes( data.status ) ? data.status : "info"
         addToast(`${data.coin} ${data.details}`, { appearance, autoDismiss: true })
