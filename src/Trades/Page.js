1b110932f709d98f73294030d27bcdbfcb75786b
Tim Bijnen <bezigebijnen@gmail.com>
2021-10-16 01:08:38 +0200

    Fix

--- src/Trades/Page.js
+++ src/Trades/Page.js
@@ -1,5 +1,5 @@
 import useTrades from "./hooks/trades"
-import { Card } from "react-bootstrap"
+import { Card, Container, Row, Col } from "react-bootstrap"
 import { Led } from "../Icon"
 
 const Leds = ( ( { a, b, c } ) => (
@@ -18,12 +18,16 @@ const Page = () => {
     }
     
     items.forEach( ( i ) => {
-        if ( !grouped[ i.candle_id ] ) {
-            grouped[ i.candle_id ] = []
+        if ( !grouped[ i.symbol ] ) {
+            grouped[ i.symbol ] = {}
+        }
+        if ( !grouped[ i.symbol ][ i.candle_id ] ) {
+            grouped[ i.symbol ][ i.candle_id ] = []
         } 
-        grouped[ i.candle_id ] = [ ...grouped[ i.candle_id ], i ]
+        grouped[ i.symbol ][ i.candle_id ] = [ ...grouped[ i.symbol ][ i.candle_id ], i ]
+
     } )
-    const orders = Object.entries(grouped)
+    const symbols = Object.entries(grouped)
 
     const amtOfSellOrders = items.filter( ( i ) => i.type === "SELL" ).length
     const amtOfMarketOrders = items.filter( ( i ) => i.type === "MARKET" ).length
@@ -57,77 +61,83 @@ const Page = () => {
                 <div>
                     Sell orders: { amtOfSellOrders } ({ (amtOfSellOrders / amtOfOrders * 100).toFixed(2) }%)<br/>
                     <div className="d-flex">
-                        <Leds /><div className="w-100">: { amtOfSellOrdersNone }</div>
+                        <Leds /><div className="w-100">: { amtOfSellOrdersNone } ({ (amtOfSellOrdersNone / (amtOfSellOrdersNone + amtOfMarketOrdersNone ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a/><div className="w-100">: { amtOfSellOrders1h }</div>
+                        <Leds a/><div className="w-100">: { amtOfSellOrders1h } ({ (amtOfSellOrders1h / (amtOfSellOrders1h + amtOfMarketOrders1h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds b/><div className="w-100">: { amtOfSellOrders4h }</div>
+                        <Leds b/><div className="w-100">: { amtOfSellOrders4h } ({ (amtOfSellOrders4h / (amtOfSellOrders4h + amtOfMarketOrders4h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds c/><div className="w-100">: { amtOfSellOrders24h }</div>
+                        <Leds c/><div className="w-100">: { amtOfSellOrders24h } ({ (amtOfSellOrders24h / (amtOfSellOrders24h + amtOfMarketOrders24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a b/><div className="w-100">: { amtOfSellOrders1h4h }</div>
+                        <Leds a b/><div className="w-100">: { amtOfSellOrders1h4h } ({ (amtOfSellOrders1h4h / (amtOfSellOrders1h4h + amtOfMarketOrders1h4h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a c/><div className="w-100">: { amtOfSellOrders1h24h }</div>
+                        <Leds a c/><div className="w-100">: { amtOfSellOrders1h24h } ({ (amtOfSellOrders1h24h / (amtOfSellOrders1h24h + amtOfMarketOrders1h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds b c/><div className="w-100">: { amtOfSellOrders4h24h }</div>
+                        <Leds b c/><div className="w-100">: { amtOfSellOrders4h24h } ({ (amtOfSellOrders4h24h / (amtOfSellOrders4h24h + amtOfMarketOrders4h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a b c/><div className="w-100">: { amtOfSellOrders1h4h24h }</div>
+                        <Leds a b c/><div className="w-100">: { amtOfSellOrders1h4h24h } ({ (amtOfSellOrders1h4h24h / (amtOfSellOrders1h4h24h + amtOfMarketOrders1h4h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                 </div>
                 <div>
                     Market orders: { amtOfMarketOrders } ({ (amtOfMarketOrders / amtOfOrders * 100).toFixed(2) }%)<br/>
                     <div className="d-flex">
-                        <Leds /><div className="w-100">: { amtOfMarketOrdersNone }</div>
+                        <Leds /><div className="w-100">: { amtOfMarketOrdersNone } ({ (amtOfMarketOrdersNone / (amtOfMarketOrdersNone + amtOfSellOrdersNone ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a/><div className="w-100">: { amtOfMarketOrders1h }</div>
+                        <Leds a/><div className="w-100">: { amtOfMarketOrders1h } ({ (amtOfMarketOrders1h / (amtOfMarketOrders1h + amtOfSellOrders1h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds b/><div className="w-100">: { amtOfMarketOrders4h }</div>
+                        <Leds b/><div className="w-100">: { amtOfMarketOrders4h } ({ (amtOfMarketOrders4h / (amtOfMarketOrders4h + amtOfSellOrders4h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds c/><div className="w-100">: { amtOfMarketOrders24h }</div>
+                        <Leds c/><div className="w-100">: { amtOfMarketOrders24h } ({ (amtOfMarketOrders24h / (amtOfMarketOrders24h + amtOfSellOrders24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a b/><div className="w-100">: { amtOfMarketOrders1h4h }</div>
+                        <Leds a b/><div className="w-100">: { amtOfMarketOrders1h4h } ({ (amtOfMarketOrders1h4h / (amtOfMarketOrders1h4h + amtOfSellOrders1h4h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a c/><div className="w-100">: { amtOfMarketOrders1h24h }</div>
+                        <Leds a c/><div className="w-100">: { amtOfMarketOrders1h24h } ({ (amtOfMarketOrders1h24h / (amtOfMarketOrders1h24h + amtOfSellOrders1h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds b c/><div className="w-100">: { amtOfMarketOrders4h24h }</div>
+                        <Leds b c/><div className="w-100">: { amtOfMarketOrders4h24h } ({ (amtOfMarketOrders4h24h / (amtOfMarketOrders4h24h + amtOfSellOrders4h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                     <div className="d-flex">
-                        <Leds a b c/><div className="w-100">: { amtOfMarketOrders1h4h24h }</div>
+                        <Leds a b c/><div className="w-100">: { amtOfMarketOrders1h4h24h } ({ (amtOfMarketOrders1h4h24h / (amtOfMarketOrders1h4h24h + amtOfSellOrders1h4h24h ) * 100).toFixed(2) }%)</div>
                     </div>
                 </div>
             </div>
             
             <br />
             <hr />
-            { orders.map( ( [candle_id, trades] ) => (
-                <Card>
-                    { candle_id }
-                    { trades.map( ( t ) => (
-                        <div>
-                            { t.order_id }
-                            { t.symbol }
-                            { t.type }
-                            { t.coin }
-                            { t.original_qty }
-                            { t.executed_qty }
-                            { t.cancelled ? "Inactive" : "Active"}
-                        </div>
-                    ) ) }
-                </Card>
-            ) ) }
+            <Container>
+                <Row>
+                        { symbols.map( ( [ symbol, orders ] ) => (
+                    <Col xs={2}>
+                                <Card>
+                            <div className="d-inline-block mb-2">
+                                <b>{ symbol }</b>
+                                {
+                                    Object.entries(orders).map( ( [candle_id, trades] ) => (
+                                        <div>
+                                            { trades.length === 1 ? "Active" : null }
+                                            { trades.length === 2 ? "Sell" : null }
+                                            { trades.length === 3 ? "Market" : null }
+                                        </div>
+                                            ) )
+                                        }
+                            </div>
+                                        </Card>
+                    </Col>
+                        ) ) }
+                </Row>
+            </Container>
         </>
     )
 }
