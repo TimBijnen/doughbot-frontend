027c22c14a9d2853ae79750507c05799db7d6384
Tim Bijnen <bezigebijnen@gmail.com>
2021-10-16 01:44:13 +0200

    Fix

--- src/Trades/Info.js
+++ src/Trades/Info.js
@@ -5,13 +5,13 @@ const TradeInfo = ( { symbol, orders } ) => {
     const sell = Object.entries(orders).reduce( ( result, [cid, trades] ) => result + (trades.length === 2 ? 1 : 0), 0)
     const market = Object.entries(orders).reduce( ( result, [cid, trades] ) => result + (trades.length === 3 ? 1 : 0), 0)
     let bg = "secondary"
-    if ( sell + market === 0 ) {
-        return null
-    }
+    // if ( sell + market === 0 ) {
+    //     return null
+    // }
     if ( sell > market ) {
         bg = "primary"
     } 
-    if ( sell === market ) {
+    if ( sell === market && sell !== 0) {
         bg = "warning"
     }
     if ( sell / ( sell + market ) > 0.8 && sell + market > 10) {
@@ -24,7 +24,7 @@ const TradeInfo = ( { symbol, orders } ) => {
         <Col xs={6} sm={4} md={3} lg={2} className="mb-2">
         <Card>
     <div className="d-inline-block mb-2">
-        <b>{ symbol } ( { ( sell / ( sell + market ) * 100 ).toFixed( 2 ) }% )</b><Badge bg={ bg } className="float-end">{ sell + market }</Badge><br />
+        <b>{ symbol } ( { (( sell / ( sell + market ) * 100 ) || 0).toFixed( 2 ) }% )</b><Badge bg={ bg } className="float-end">{ sell + market }</Badge><br />
         Active: { active }<br />
         Sell: { sell }<br />
         Market: { market }<br />
