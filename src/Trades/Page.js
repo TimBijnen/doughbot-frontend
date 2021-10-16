ae0c673be2988d71baf5e0ed8d960de507ef80c7
Tim Bijnen <bezigebijnen@gmail.com>
2021-10-16 14:29:38 +0200

    Fix

--- src/Trades/Page.js
+++ src/Trades/Page.js
@@ -18,7 +18,11 @@ const Page = () => {
         return <div>Loading</div>
     }
     
+    const perCandle = {}
     items.forEach( ( i ) => {
+        if ( !perCandle[ i.candle_id ] ) {
+            perCandle[ i.candle_id ] = []
+        }
         if ( !grouped[ i.symbol ] ) {
             grouped[ i.symbol ] = {}
         }
@@ -26,33 +30,35 @@ const Page = () => {
             grouped[ i.symbol ][ i.candle_id ] = []
         } 
         grouped[ i.symbol ][ i.candle_id ] = [ ...grouped[ i.symbol ][ i.candle_id ], i ]
+        perCandle[ i.candle_id ] = [ ...perCandle[ i.candle_id ], i ]
 
     } )
     const symbols = Object.entries(grouped)
-
-    const amtOfSellOrders = items.filter( ( i ) => i.type === "SELL" ).length
-    const amtOfMarketOrders = items.filter( ( i ) => i.type === "MARKET" ).length
+    const _items = Object.entries( perCandle ).reduce( ( result, [ candle_id, item ] ) => [ ...result, { candle_id, type: item[item.length - 1].type, one_h: item[0].one_h, four_h: item[0].four_h, twofour_h: item[0].twofour_h } ], [] ) 
+    // const _items = items
+    const amtOfSellOrders = _items.filter( ( i ) => i.type === "SELL" ).length
+    const amtOfMarketOrders = _items.filter( ( i ) => i.type === "MARKET" ).length
     const amtOfOrders = amtOfMarketOrders + amtOfSellOrders
     
-    const amtOfSellOrdersNone = items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h < 100 && i.twofour_h < 100 ).length
-    const amtOfSellOrders1h = items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h < 100 ).length
-    const amtOfSellOrders4h = items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
-    const amtOfSellOrders24h = items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
+    const amtOfSellOrdersNone = _items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h < 100 && i.twofour_h < 100 ).length
+    const amtOfSellOrders1h = _items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h < 100 ).length
+    const amtOfSellOrders4h = _items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
+    const amtOfSellOrders24h = _items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
 
-    const amtOfSellOrders1h4h = items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
-    const amtOfSellOrders1h24h = items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
-    const amtOfSellOrders4h24h = items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
-    const amtOfSellOrders1h4h24h = items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
+    const amtOfSellOrders1h4h = _items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
+    const amtOfSellOrders1h24h = _items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
+    const amtOfSellOrders4h24h = _items.filter( ( i ) => i.type === "SELL" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
+    const amtOfSellOrders1h4h24h = _items.filter( ( i ) => i.type === "SELL" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
     
     
-    const amtOfMarketOrdersNone = items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h < 100 && i.twofour_h < 100 ).length
-    const amtOfMarketOrders1h = items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h < 100 ).length
-    const amtOfMarketOrders4h = items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
-    const amtOfMarketOrders24h = items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
-    const amtOfMarketOrders1h4h = items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
-    const amtOfMarketOrders1h24h = items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
-    const amtOfMarketOrders4h24h = items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
-    const amtOfMarketOrders1h4h24h = items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
+    const amtOfMarketOrdersNone = _items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h < 100 && i.twofour_h < 100 ).length
+    const amtOfMarketOrders1h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h < 100 ).length
+    const amtOfMarketOrders4h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
+    const amtOfMarketOrders24h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
+    const amtOfMarketOrders1h4h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h < 100 ).length
+    const amtOfMarketOrders1h24h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h < 100 && i.twofour_h >= 100 ).length
+    const amtOfMarketOrders4h24h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h < 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
+    const amtOfMarketOrders1h4h24h = _items.filter( ( i ) => i.type === "MARKET" && i.one_h >= 100 && i.four_h >= 100 && i.twofour_h >= 100 ).length
     
     return (
         <>
