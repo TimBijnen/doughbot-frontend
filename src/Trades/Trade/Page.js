66679ec78775ad1561ba75eacacf9cb964e66976
Tim Bijnen <bezigebijnen@gmail.com>
2021-12-20 14:51:44 +0100

    Add page

--- src/Trades/Trade/Page.js
+++ src/Trades/Trade/Page.js
@@ -1,32 +1,33 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import axios from "axios"
 
 const API = process.env.REACT_APP_API_URL
 
-const trade = {
-    "buy_orders": 2,
-    "candle_id": null,
-    "created_at": "Sun, 19 Dec 2021 09:25:07 GMT",
-    "id": 1,
-    "sell_orders": 2,
-    "sentiment_1h": 99.80223254985493,
-    "sentiment_24h": 101.79285957749812,
-    "sentiment_4h": 99.84041778614629,
-    "symbol": "KMDBTC",
-    "total_bought_coins": 33.0,
-    "total_buy_value": 0.00051255,
-    "total_sell_value": 0,
-    "total_sold_coins": 0.0
-}
+// const trade = {
+//     "buy_orders": 2,
+//     "candle_id": null,
+//     "created_at": "Sun, 19 Dec 2021 09:25:07 GMT",
+//     "id": 1,
+//     "sell_orders": 2,
+//     "sentiment_1h": 99.80223254985493,
+//     "sentiment_24h": 101.79285957749812,
+//     "sentiment_4h": 99.84041778614629,
+//     "symbol": "KMDBTC",
+//     "total_bought_coins": 33.0,
+//     "total_buy_value": 0.00051255,
+//     "total_sell_value": 0,
+//     "total_sold_coins": 0.0
+// }
 
 const TradePage = () => {
     const { id } = useParams();
-    
+    const [ trade, setTrade ] = useState( {} )
+
     useEffect(() => {
         async function loadData() {
             const { data } = await axios.get( `${ API }/trades/${ id }` )
-            console.log(data)
+            setTrade( data.data )
         }
         loadData()
     }, [ id ])
