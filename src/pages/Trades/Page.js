1cadd130bf72a0733a1736e45db2faba32f84090
tim <bezigebijnen@gmail.com>
2022-03-10 00:41:14 +0100

    Add styling

--- src/pages/Trades/Page.js
+++ src/pages/Trades/Page.js
@@ -14,12 +14,30 @@ const TradesPage = () => {
                 ) ) }
             </Row>
             <CallStats isLoading={ isLoading } error={ error }>
-                { trades.map( ( trade ) => (
-                    <div>
-                        { trade.symbol }
-                        { trade.created_at }
-                    </div>
-                ) ) }
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Symbol</th>
+                            <th scope="col">Created at</th>
+                            <th scope="col">Result</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { trades.map( ( trade ) => (
+                            <tr>
+                                <th>
+                                    { trade.symbol }
+                                </th>
+                                <td>
+                                    { trade.created_at }
+                                </td>
+                                <td>
+                                    { trade.total_buy_value / trade.total_sell_value }
+                                </td>
+                            </tr>
+                        ) ) }
+                    </tbody>
+                </table>
             </CallStats>
         </>
     )
